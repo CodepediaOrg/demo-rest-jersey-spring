cbb12e6d8d494e97223c80e1d91643e050c8a290
amacoder <ama@codingpedia.org>
2014-11-25 16:11:41 +0100

    corrected blog post url

--- src/main/java/org/codingpedia/demo/rest/filters/AppConstants.java
+++ src/main/java/org/codingpedia/demo/rest/filters/AppConstants.java
@@ -3,6 +3,6 @@ package org.codingpedia.demo.rest.filters;
 public class AppConstants {
 	
 	public static final int GENERIC_APP_ERROR_CODE = 5001;		
-	public static final String BLOG_POST_URL = "here will be the blo post url";
+	public static final String BLOG_POST_URL = "http://www.codingpedia.org/ama/tutorial-rest-api-design-and-implementation-in-java-with-jersey-and-spring/";
 
 }
