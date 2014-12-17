6af8da82d40964aea83d8816090513a3dd3e5012
amacoder <ama@codingpedia.org>
2014-12-17 15:52:30 +0100

    added mockito test class

---
+++ src/test/java/org/codingpedia/demo/rest/service/PodcastServiceDbAccessImplTest.java
@@ -0,0 +1,22 @@
+package org.codingpedia.demo.rest.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PodcastServiceDbAccessImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCreatePodcast() {
+		fail("Not yet implemented");
+	}
+
+}
