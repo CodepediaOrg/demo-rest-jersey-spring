eab8c0e1a8cd8bb46d9a09bebf01a0e75e26d2ea
amacoder <ama@codingpedia.org>
2015-01-02 15:49:48 +0100

    Added verification step for Mockito test and an extra test method

--- src/test/java/org/codingpedia/demo/rest/service/PodcastServiceDbAccessImplTest.java
+++ src/test/java/org/codingpedia/demo/rest/service/PodcastServiceDbAccessImplTest.java
@@ -1,7 +1,6 @@
 package org.codingpedia.demo.rest.service;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.codingpedia.demo.rest.dao.PodcastDao;
 import org.codingpedia.demo.rest.dao.PodcastEntity;
@@ -23,6 +22,7 @@ public class PodcastServiceDbAccessImplTest {
 	private static final String SOME_FEED = "some_feed";
 	private static final String SOME_TITLE = "some title";
 	private static final String EXISTING_FEED = "http://quarks.de/feed";
+	private static final Long SOME_ID = 13L;
 	
 	@Rule
 	public ExpectedException exception = ExpectedException.none();	
@@ -48,7 +48,16 @@ public class PodcastServiceDbAccessImplTest {
 		podcast.setFeed(SOME_FEED);
 		podcast.setTitle(SOME_TITLE);
 		Long createPodcast = sut.createPodcast(podcast);
-
+		
+		verify(podcastDao).getPodcastByFeed(SOME_FEED);//verifies if the method podcastDao.getPodcastByFeed has been called exactly once with that exact input parameter
+		verify(podcastDao, times(1)).getPodcastByFeed(SOME_FEED);//same as above
+		verify(podcastDao, times(1)).getPodcastByFeed(eq(SOME_FEED));//same as above
+		verify(podcastDao, times(1)).getPodcastByFeed(anyString());//verifies if the method podcastDao.getPodcastByFeed has been called exactly once with any string as input
+		verify(podcastDao, atLeastOnce()).getPodcastByFeed(SOME_FEED);//verifies if the method podcastDao.getPodcastByFeed has been called at least once with that exact input parameter		
+		verify(podcastDao, atLeast(1)).getPodcastByFeed(SOME_FEED);//verifies if the method podcastDao.getPodcastByFeed has been called at least once with that exact input parameter
+		verify(podcastDao, times(1)).createPodcast(any(PodcastEntity.class));
+		verify(podcastDao, never()).getLegacyPodcastById(anyLong());//verifies the method podcastDao.getLegacyPodcastById has never been called
+		
 		Assert.assertTrue(createPodcast == CREATED_PODCAST_RESOURCE_ID);
 	}
 
@@ -85,6 +94,44 @@ public class PodcastServiceDbAccessImplTest {
 		podcast.setFeed(EXISTING_FEED);
 		sut.createPodcast(podcast);
 
+	}	
+	
+
+	@Test
+	public void testUpdatePartiallyPodcast_successful() throws AppException {
+		
+		PodcastEntity podcastEntity = new PodcastEntity();
+		podcastEntity.setId(SOME_ID);
+		when(podcastDao.getPodcastById(SOME_ID)).thenReturn(podcastEntity);		
+		doNothing().when(podcastDao).updatePodcast(any(PodcastEntity.class));
+		
+		Podcast podcast = new Podcast(podcastEntity);
+		podcast.setFeed(SOME_FEED);
+		podcast.setTitle(SOME_TITLE);
+		sut.updatePartiallyPodcast(podcast);
+		
+		verify(podcastDao).getPodcastById(SOME_ID);//verifies if the method podcastDao.getPodcastById has been called exactly once with that exact input parameter
+		verify(podcastDao).updatePodcast(any(PodcastEntity.class));		
+		
+		Assert.assertTrue(podcast.getFeed() == SOME_FEED);
+		Assert.assertTrue(podcast.getTitle() == SOME_TITLE);
+	}
+	
+	@Test
+	public void testUpdatePartiallyPodcast_not_existing_podcast() {
+		
+		when(podcastDao.getPodcastById(SOME_ID)).thenReturn(null);
+		
+		Podcast podcast = new Podcast();
+		podcast.setId(SOME_ID);
+		try {
+			sut.updatePartiallyPodcast(podcast);
+			Assert.fail("Should have thrown an exception"); 
+		} catch (AppException e) {
+			verify(podcastDao).getPodcastById(SOME_ID);//verifies if the method podcastDao.getPodcastById has been called exactly once with that exact input parameter
+			Assert.assertEquals(e.getCode(), 404);
+		}
+		
 	}	
 	
 }
